46cb5b480bc54d7d51d3e440056bc15c84fa127b
Emre Emiroğlu <emirogluemre08@gmail.com>
2025-11-17 19:36:01 +0300

    latest

---
+++ Editor/AbilityCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AbilitySystem.Editor
+{
+    public sealed class AbilityCreator : EditorWindow
+    {
+        #region Core
+        [MenuItem("Tools/Ability Creator")]
+        private static void ShowWindow()
+        {
+            AbilityCreator window = GetWindow<AbilityCreator>();
+            
+            window.titleContent = new GUIContent("Ability Creator");
+            window.minSize = new Vector2(480, 270);
+            window.maxSize = new Vector2(960, 540);
+            
+            window.Show();
+        }
+        #endregion
+    }
+}
